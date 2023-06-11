58941ded79a7026bf0d958ba31b2d19c1fbb19ef
Imran-A-K <imran.k.professional@gmail.com>
2023-06-11 12:21:49 +0600

    successfully added make instructor feature

--- src/pages/dashboard/Admin/AdminTableRow.jsx
+++ src/pages/dashboard/Admin/AdminTableRow.jsx
@@ -1,7 +1,7 @@
 import { FaChalkboardTeacher,FaUserShield } from "react-icons/fa";
 
 
-const AdminTableRow = ({ user,id,imageUrl, name, role, serial,email,makeAdmin }) => {
+const AdminTableRow = ({ user,id,imageUrl, name, role, serial,email,makeAdmin,makeInstructor }) => {
 
     
   return (
@@ -21,8 +21,9 @@ const AdminTableRow = ({ user,id,imageUrl, name, role, serial,email,makeAdmin })
     <td className="text-center">{name}</td>
     <td className="text-center">{email}</td>
     <td className="text-center">{role}</td>
-    <td className="text-center"><button disabled={role === "instructor" ? true : false} className="btn bg-slate-800 hover:bg-amber-500 hover:text-gray-800 text-yellow-200 text-lg"><FaChalkboardTeacher /></button></td>
-    <td className="text-center"><button disabled={role === "admin" ? true : false} 
+    <td className="text-center"><button onClick={() => makeInstructor(name,id)} disabled={role === "instructor" ? true : false} className="btn bg-slate-800 hover:bg-amber-500 hover:text-gray-800 text-yellow-200 text-lg"><FaChalkboardTeacher /></button></td>
+    <td className="text-center"><button 
+    disabled={role === "admin" ? true : false} 
     onClick={() => makeAdmin(name,id)} 
     className="btn bg-cyan-600 text-yellow-200 hover:bg-red-600 hover:text-gray-800 text-lg"><FaUserShield /></button></td>
   </tr>
--- src/pages/dashboard/Admin/ManageUsers.jsx
+++ src/pages/dashboard/Admin/ManageUsers.jsx
@@ -1,5 +1,5 @@
 import useAuthentication from "../../../hooks/useAuthentication";
-import useGetUsers from "../../../hooks/useGetUsers"
+import useGetUsers from "../../../hooks/useGetUsers";
 import AdminTableRow from "./AdminTableRow";
 import useAxiosInterceptor from "../../../hooks/useAxiosInterceptor";
 import Swal from "sweetalert2";
@@ -7,40 +7,63 @@ import Swal from "sweetalert2";
 const ManageUsers = () => {
   const [allUsers, refetch] = useGetUsers();
   const [axiosBase] = useAxiosInterceptor();
-  const {user}= useAuthentication();
-  
+  const { user } = useAuthentication();
+
   // console.log(allUsers)
-  const makeAdmin = (name,id) => {
+  const makeAdmin = (name, id) => {
     Swal.fire({
-      title: 'Are you sure?',
+      title: "Are you sure?",
       text: `${name} is going to be a new Admin!`,
-      icon: 'warning',
+      icon: "warning",
       showCancelButton: true,
-      confirmButtonColor: '#3085d6',
-      cancelButtonColor: '#d33',
-      confirmButtonText: 'Yes, I am sure!'
-    }).then(async(result) => {
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, I am sure!",
+    }).then(async (result) => {
       if (result.isConfirmed) {
-        await axiosBase.patch(`/users/admin/${id}`)
-        .then(response =>{
-          if(response.data.modifiedCount){
-            refetch();    
+        await axiosBase.patch(`/users/make-admin/${id}`).then((response) => {
+          if (response.data.modifiedCount) {
+            refetch();
             Swal.fire({
-              position: 'center',
-              icon: 'success',
+              position: "center",
+              icon: "success",
               title: `The ${name} is now an Admin`,
               showConfirmButton: false,
-              timer: 1500
-            })
+              timer: 1500,
+            });
           }
-        })
-        
+        });
       }
-    })
-    
-    
+    });
+  };
 
-  }
+  const makeInstructor = (name, id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: `${name} is going to be a new Instructor!`,
+      icon: "question",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, I am sure!",
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        await axiosBase.patch(`/users/make-instructor/${id}`).then((response) => {
+          if (response.data.modifiedCount) {
+            refetch();
+            Swal.fire({
+              position: "center",
+              icon: "success",
+              title: `The ${name} is now an Instructor`,
+              showConfirmButton: false,
+              timer: 1500,
+            });
+          }
+        });
+      }
+    });
+  };
+
   return (
     <div className="">
       <h1 className="text-center text-4xl py-9 font-bold text-indigo-600">
@@ -60,25 +83,25 @@ const ManageUsers = () => {
             </tr>
           </thead>
           <tbody>
-           {
-            allUsers.map((eachUser, index) => <AdminTableRow
-            key={eachUser._id}
-            serial={index+1}
-            name={eachUser.name}
-            email={eachUser.email}
-            id={eachUser._id}
-            role={eachUser.role}
-            imageUrl={eachUser.photoUrl}
-            user={user}
-            makeAdmin={makeAdmin}
-            ></AdminTableRow>)  
-                     }
-            
+            {allUsers.map((eachUser, index) => (
+              <AdminTableRow
+                key={eachUser._id}
+                serial={index + 1}
+                name={eachUser.name}
+                email={eachUser.email}
+                id={eachUser._id}
+                role={eachUser.role}
+                imageUrl={eachUser.photoUrl}
+                user={user}
+                makeAdmin={makeAdmin}
+                makeInstructor={makeInstructor}
+              ></AdminTableRow>
+            ))}
           </tbody>
         </table>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ManageUsers
+export default ManageUsers;
